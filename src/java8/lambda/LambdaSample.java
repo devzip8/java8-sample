49a1765965acd3fcf7619939aa69f168c1d3eef0
dev-neko <pi3kp85@gmail.com>
2015-10-11 16:05:49 +0900

    Add samples of 'allMatch' and 'anyMatch'

--- src/java8/lambda/LambdaSample.java
+++ src/java8/lambda/LambdaSample.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Sample for Lambda
+ * 
+ * @author dev-neko
+ * @since 2015/10/11
+ */
 public class LambdaSample {
 
 	public LambdaSample(){
@@ -16,8 +22,6 @@ public class LambdaSample {
 				"nagoya",
 				"kyoto",
 				"kobe",
-				"kobe",
-				"nagasaki",
 				"nagasaki",
 				"fukuoka",
 				"chitose",
@@ -31,7 +35,7 @@ public class LambdaSample {
 		
 		main.sampleUpperCase(list);
 		main.sampleUpperCase2(list);
-		
+		main.sampleShorterThan(list);
 	}
 	
 	private void printlnAll(List<String> list){
@@ -65,4 +69,22 @@ public class LambdaSample {
 		return newList;
 	}
 	
+	private void sampleShorterThan(List<String> list){
+		System.out.println("# sampleShorterThan ");
+		System.out.println(isAllShorterThan(list, 9)); 
+		System.out.println(isAllShorterThan(list, 8));
+
+		System.out.println(isAnyShorterThan(list, 5));
+		System.out.println(isAnyShorterThan(list, 4));
+	}
+	
+	// Try to use 'allMatch'
+	private boolean isAllShorterThan(List<String> list, int length){
+		return list.stream().allMatch(e -> e.length() < length);
+	}
+	
+	// Try to use 'anyMatch'
+	private boolean isAnyShorterThan(List<String> list, int length){
+		return list.stream().anyMatch(e -> e.length() < length);
+	}
 }
